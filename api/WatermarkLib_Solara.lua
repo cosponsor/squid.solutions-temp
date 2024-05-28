7d645a63178362ad2653afbf9a5fe86d90bc72ae
mew <68041302+iuml@users.noreply.github.com>
2024-05-28 02:09:02 -0600

    Add files via upload

---
+++ api/WatermarkLib_Solara.lua
@@ -0,0 +1,42 @@
+local Library = {}
+
+function MainFunction(Text)
+	local Squiddy = Instance.new("ScreenGui")
+	local TextLabel = Instance.new("TextLabel")
+	local UITextSizeConstraint = Instance.new("UITextSizeConstraint")
+	local UIAspectRatioConstraint = Instance.new("UIAspectRatioConstraint")
+	Squiddy.Name = "Squid !"
+	Squiddy.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+	Squiddy.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+	Squiddy.DisplayOrder = 999999999
+	TextLabel.Parent = Squiddy
+	TextLabel.AnchorPoint = Vector2.new(0.5, 0.5)
+	TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+	TextLabel.BackgroundTransparency = 1.000
+	TextLabel.BorderColor3 = Color3.fromRGB(0, 0, 0)
+	TextLabel.BorderSizePixel = 0
+	TextLabel.Position = UDim2.new(0.914253891, 0, 0.5, 0)
+	TextLabel.Size = UDim2.new(0.181684986, 1, 0.0452090763, 1)
+	TextLabel.Font = Enum.Font.RobotoMono
+	TextLabel.Text = Text
+	TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
+	TextLabel.TextScaled = true
+	TextLabel.TextSize = 14.000
+	TextLabel.TextStrokeTransparency = 0.000
+	TextLabel.TextWrapped = true
+	UITextSizeConstraint.Parent = TextLabel
+	UITextSizeConstraint.MaxTextSize = 14
+	UIAspectRatioConstraint.Parent = Squiddy
+	UIAspectRatioConstraint.AspectRatio = 1.711
+end
+
+function Library:CreateWatermark(Txt)
+	if not game:GetService('Players').LocalPlayer.PlayerGui:FindFirstChild("Squid !") then
+		MainFunction(Txt)
+	else
+		game:GetService('Players').LocalPlayer.PlayerGui:FindFirstChild("Squid !"):Destroy()
+		MainFunction(Txt)
+	end
+end
+
+return Library
